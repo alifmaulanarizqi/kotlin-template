69d1dc80b133ad75c237f50d266245f2e2c1ffef
alif <alifmaulana312@gmail.com>
2025-12-19 14:27:22 +0700

    feat: update package

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,12 +9,12 @@ plugins {
 
 android {
     namespace = "com.example.kotlintemplate"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.example.kotlintemplate"
         minSdk = 24
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
@@ -124,11 +124,11 @@ dependencies {
     debugImplementation(libs.androidx.ui.test.manifest)
 
     // CameraX
-    implementation("androidx.camera:camera-core:1.5.2")
-    implementation("androidx.camera:camera-camera2:1.5.2")
-    implementation("androidx.camera:camera-lifecycle:1.5.2")
-    implementation("androidx.camera:camera-view:1.3.4")
+    implementation(libs.androidx.camera.core)
+    implementation(libs.androidx.camera.camera2)
+    implementation(libs.androidx.camera.lifecycle)
+    implementation(libs.androidx.camera.view)
 
     // ML Kit Barcode
-    implementation("com.google.mlkit:barcode-scanning:17.3.0")
+    implementation(libs.google.mlkit.barcode)
 }
